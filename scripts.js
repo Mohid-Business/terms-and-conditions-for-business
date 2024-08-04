47c6211f9585e6b4f26ac32342735e7f2d085d33
Mohid-Business <148129888+Mohid-Business@users.noreply.github.com>
2024-08-04 23:35:32 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 document.getElementById('agreeButton').addEventListener('click', function() {
-    window.location.href = 'YOUR_GOOGLE_DRIVE_LINK_HERE';
+    window.location.href = 'googe.com';
 });
